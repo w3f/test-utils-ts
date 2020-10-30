75adc20d4c6d64ad0a8a6586e8eecbb838911fa6
pantelis <pampatzoglou@gmail.com>
2020-10-30 18:27:38 +0200

    include polkadotrpc to patch

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -4,3 +4,4 @@ set -e
 echo patching...
 sed -i "/parity\/polkadot/c\      - image: parity\/polkadot:$latest_upstream " .circleci/config.yml
 sed -i "/const polkadotVersion/c\const polkadotVersion = '$latest_upstream'; " test/polkadotRpc.ts
+sed -i "/async start(version /c\    async start(version = '$latest_upstream'): Promise<void> {  " src/polkadotRpc.ts
